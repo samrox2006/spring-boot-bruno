e7ead4cd1f36505f66877e3b6167026bd34b60a1
Bruno Mulin <verdibianco@hotmail.com>
2023-12-26 01:14:20 -0300

    Inicio do projeto

---
+++ src/main/java/io/github/samrox2006/VendasApplication.java
@@ -0,0 +1,20 @@
+package io.github.samrox2006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return  "hello Bruno Sam";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}

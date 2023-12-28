d1543a1bc01b00631eb96e67d364fd6ddf19d92e
Bruno Mulin <verdibianco@hotmail.com>
2023-12-28 02:02:37 -0300

    Criação de das classes service, model e repository de cliente

---
+++ src/main/java/io/github/samrox2006/model/Cliente.java
@@ -0,0 +1,13 @@
+package io.github.samrox2006.model;
+
+public class Cliente {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/io/github/samrox2006/service/ClientesService.java
@@ -0,0 +1,25 @@
+package io.github.samrox2006.service;
+
+import io.github.samrox2006.model.Cliente;
+import io.github.samrox2006.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientesService {
+
+    private ClientesRepository repository;
+
+    public ClientesService( ClientesRepository repository ){
+        this.repository = repository;
+    }
+
+    public void salvarCliente(Cliente cliente){
+        validarCliente(cliente);
+        this.repository.persistir(cliente);
+    }
+
+    public void validarCliente(Cliente cliente){
+        //aplica validações
+    }
+}

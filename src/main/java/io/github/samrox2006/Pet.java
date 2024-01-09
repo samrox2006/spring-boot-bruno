a5bf0261afe40e8c83194f0d7687caf13c87042f
Bruno Mulin <verdibianco@hotmail.com>
2024-01-09 00:56:43 -0300

    Rebase do projeto

--- src/main/java/io/github/samrox2006/Pet.java
+++
@@ -1,5 +0,0 @@
-package io.github.samrox2006;
-
-public interface Pet {
-    void msgLinha();
-}
--- src/main/java/io/github/samrox2006/VendasApplication.java
+++ src/main/java/io/github/samrox2006/VendasApplication.java
@@ -1,45 +1,10 @@
 package io.github.samrox2006;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-    @Value("${application.name}")
-    private String applicationName;
-
-  @Cat
-    private Pet pet;
-
-    @Bean
-    public CommandLineRunner executar(){
-        return args -> {
-            this.pet.msgLinha();
-        };
-    }
-
-
-    @Autowired
-    @Qualifier("applicationHello")
-    private String applicationHello;
-
-    public VendasApplication() {
-    }
-
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return applicationName+"<br/>"+applicationHello ;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
